77d8e5e6c786524b3787bd834ffcbb3ead706a9e
Agustín Vargas <agustinvargas93@gmail.com>
2022-04-11 16:34:20 -0300

    add metatags

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,24 @@ import DataFromAPIs from '../src/contexts/DataFromAPIs';
 import NextUI from '../src/contexts/NextUI';
 import Script from 'next/script';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Criticar</title>
+        <meta name='description' content='Buscador de críticas de cine' />
+        <meta
+          name='keywords'
+          content='críticas de cine, buscador críticas cine, análisis cine, cine, críticas, críticas cine argentina, revista cine'
+        />
+        <meta
+          itemProp='name'
+          content='Criticar. Buscador de críticas de cine'
+        />
+        <meta itemProp='description' content='Buscador de críticas de cine' />
+      </Head>
       <Script
         strategy='lazyOnload'
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
